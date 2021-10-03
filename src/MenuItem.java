7715678a01419501e2ce6b82cef849d57dce2893
TruSloth <foo.lucas@yahoo.com.sg>
2021-10-04 03:07:28 +0800

    create MenuItem class

---
+++ src/MenuItem.java
@@ -0,0 +1,29 @@
+public class MenuItem {
+    private String name;
+    private String description;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
