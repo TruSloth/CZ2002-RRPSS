02dc701522dea7ba5816fd24bc26a9cbbbfa303c
KenKneeKnee <78136236+KenKneeKnee@users.noreply.github.com>
2021-11-11 21:06:12 +0800

    Update RemoveFromPackageCommand.java

--- src/com/CZ2002/project_commands/menu/RemoveFromPackageCommand.java
+++ src/com/CZ2002/project_commands/menu/RemoveFromPackageCommand.java
@@ -1,14 +1,19 @@
 package com.CZ2002.project_commands.menu;
 
 import com.CZ2002.project_boundaries.MenuManager;
+import com.CZ2002.project_entities.AlaCarteItem;
+import com.CZ2002.project_entities.MenuItem;
+import com.CZ2002.project_entities.PackageItem;
+import com.CZ2002.project_exceptions.InvalidMenuItemException;
+import com.CZ2002.project_interfaces.ICommand;
 
 /**
  * This class completes the 'Remove ala carte item from package' action.
  */
-public class RemoveFromPackageCommand {
+public class RemoveFromPackageCommand implements ICommand<Void, InvalidMenuItemException> {
     private MenuManager menuManager;
     private String name;
-    private String subname;
+    private String subName;
 
     /**
      * Constructor that accepts the necessary parameters for {@code execute} to successfully complete.
@@ -20,13 +25,25 @@ public class RemoveFromPackageCommand {
     public RemoveFromPackageCommand(MenuManager menuManager, String name,String subName) {
         this.menuManager = menuManager;
         this.name = name;
-        this.subname = subName;
+        this.subName = subName;
     }
 
     /**
      * Completes the 'Remove ala carte item from package' action.
      */
-    public void execute() {
-        menuManager.removeItemFromPackage(name, subname);
+    public Void execute() throws InvalidMenuItemException {
+    	try {
+    		menuManager.removeItemFromPackage(name, subName);
+    		if(!(menuManager.getItem(name) instanceof PackageItem)|| menuManager.getItem(name) ==null) {
+    			throw new InvalidMenuItemException("Requested Package Item does not exist");
+    		}
+    		if (!(menuManager.getItem(subName) instanceof AlaCarteItem)|| menuManager.getItem(subName) ==null) {
+    			throw new InvalidMenuItemException("Requested Ala Carte Item does not exist");
+    		}
+    	} finally{
+    		
+    	}
+    	
+    	return null;
     }
 }
